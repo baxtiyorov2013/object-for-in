72a065e53f019aac94d9cbcb3e951f986fef502d
baxtiyorov2013 <baxtiyorov2013>
2024-04-18 11:29:42 +0500

    fist comit

---
+++ main.js
@@ -0,0 +1,37 @@
+const person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+}
+for (const key in person) {
+    console.log(person[key]);
+}
+
+
+
+
+
+
+
+
+
+
+
+// const person = {
+//     method1: function sqrt1() {
+//         let a;
+//         a = Math.sqrt(2 + 3)
+//         console.log(a);
+//     },
+//     method2: function repaet() {
+//         for (let i = 0; i < 10; i++) {
+//             console.log("Hello mars");
+//         }
+//     },
+//     method3: function lenght() {
+//         let name = "Bahouddin";
+//         let lengh = name.length;
+//         console.log(lengh);
+//     }
+// }
+// person.method3()
